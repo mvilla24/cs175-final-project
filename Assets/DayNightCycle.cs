4d9cbedf6a23bac83827af1e78a324046feb7175
Matthew Villaroman <mvillaroman@college.harvard.edu>
2024-05-08 03:47:37 -0400

    added Day-Night cycle animation

---
+++ Assets/DayNightCycle.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+    public GameObject sun;
+    public GameObject moon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
