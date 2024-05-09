097dc02bb7a84123024ac37438102574a5bdaf43
mvilla24 <mvillaroman@college.harvard.edu>
2024-05-08 22:04:09 -0700

    added sound to the scenes

--- Assets/Scripts/Sounds/PersistingBGM.cs
+++ Assets/Scripts/Sounds/PersistingBGM.cs
@@ -1,23 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Audio;
 using UnityEngine;
 
 public class PersistingBGM : MonoBehaviour
 {
     AudioSource source;
+    public AudioMixer mixer;
+    public AudioClip[] bgmSongs;
 
-    static bool hasInstance = false;
+    // static bool hasInstance = false;
+
+    // 0 - Spring; 1 - Summer, 2 - Fall, 3 - Winter
+    public int bgmIndex = 0;
+
+    private string currentScene;
+
     // Start is called before the first frame update
-    IEnumerator Start()
+    // void Start()
+    // {
+    //     if (hasInstance)
+    //     {
+    //         Destroy(gameObject);
+    //     }
+    //     hasInstance = true;
+    //     DontDestroyOnLoad(gameObject);
+    //     // yield return new WaitForSeconds(2f);
+    // }
+
+    void Start()
     {
-        if (hasInstance)
-        {
-            Destroy(gameObject);
-        }
-        hasInstance = true;
         source = GetComponent<AudioSource>();
-        DontDestroyOnLoad(gameObject);
-        yield return new WaitForSeconds(2f);
+        source.clip = bgmSongs[bgmIndex];
+        mixer.SetFloat("BGMVolume", Mathf.Log10(0.0f) * 20);
+        StartSong();
+    }
+
+    public void StartSong()
+    {
+        UpdateClipIndex();
+        source.clip = bgmSongs[bgmIndex];
+        StartCoroutine(FadeMixerGroup.StartFade(mixer, "BGMVolume", 0.5f, 1.0f));
         source.Play();
+    }
+
+    public void EndSong()
+    {
+        if (currentScene == GameData.sceneToLoad)
+        {
+            return;
+        }
+        StartCoroutine(FadeMixerGroup.StartFade(mixer, "BGMVolume", 0.5f, 0.0f));
+    }
+
+    public void UpdateClipIndex()
+    {
+        if (GameData.sceneToLoad == GameData.springScene)
+        {
+            bgmIndex = 0;
+        }
+        else if (GameData.sceneToLoad == GameData.summerScene)
+        {
+            bgmIndex = 1;
+        }
+        if (GameData.sceneToLoad == GameData.fallScene)
+        {
+            bgmIndex = 2;
+        }
+        if (GameData.sceneToLoad == GameData.winterScene)
+        {
+            bgmIndex = 3;
+        }
+        currentScene = GameData.sceneToLoad;
     }
 }
---
+++ Assets/Scripts/Sounds/PersistingSFX.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class PersistingSFX : MonoBehaviour
+{
+    AudioSource source;
+    public AudioMixer mixer;
+    public AudioClip[] sfxSongs;
+
+    // static bool hasInstance = false;
+
+    // 0 - Spring; 1 - Summer, 2 - Fall, 3 - Winter
+    public int sfxIndex = 0;
+
+    private string currentScene;
+
+    // Start is called before the first frame update
+    // void Start()
+    // {
+    //     if (hasInstance)
+    //     {
+    //         Destroy(gameObject);
+    //     }
+    //     hasInstance = true;
+    //     DontDestroyOnLoad(gameObject);
+    //     // yield return new WaitForSeconds(2f);
+    // }
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        source.clip = sfxSongs[sfxIndex];
+        mixer.SetFloat("SFXVolume", Mathf.Log10(0.0f) * 20);
+        StartSong();
+    }
+
+    public void StartSong()
+    {
+        UpdateClipIndex();
+        // temp fix for no SFX
+        if (sfxIndex != 3)
+        {
+            source.clip = sfxSongs[sfxIndex];
+            StartCoroutine(FadeMixerGroup.StartFade(mixer, "SFXVolume", 0.5f, 1.0f));
+            source.Play();
+        }
+        else
+        {
+            StartCoroutine(FadeMixerGroup.StartFade(mixer, "SFXVolume", 0.5f, 1.0f));
+        }
+    }
+
+    public void EndSong()
+    {
+        if (currentScene == GameData.sceneToLoad)
+        {
+            return;
+        }
+        StartCoroutine(FadeMixerGroup.StartFade(mixer, "SFXVolume", 0.5f, 0.0f));
+    }
+
+    public void UpdateClipIndex()
+    {
+        if (GameData.sceneToLoad == GameData.springScene)
+        {
+            sfxIndex = 0;
+        }
+        else if (GameData.sceneToLoad == GameData.summerScene)
+        {
+            sfxIndex = 1;
+        }
+        if (GameData.sceneToLoad == GameData.fallScene)
+        {
+            sfxIndex = 2;
+        }
+        if (GameData.sceneToLoad == GameData.winterScene)
+        {
+            sfxIndex = 3;
+        }
+        currentScene = GameData.sceneToLoad;
+    }
+}
